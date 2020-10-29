cd06c9a7718322cc9850399f45e0c280a211f50c
fletchgraham <sfletchgraham@gmail.com>
2020-10-28 22:17:56 -0400

    refine styling

--- ice_sheet.py
+++ ice_sheet.py
@@ -1,14 +1,18 @@
+# 1-Dimensional Ice Sheet Model
+# By Fletcher Graham
+# Python 3.7
+
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
-num_cells = 10 # number of buckets
+# parameters:
+num_cells = 10 # number of cells
 domain_width = 1e6 # meters
 time_step = 100 # years
-num_years = 20000 # years
+num_years = 20000 # years (per the prompt specs)
 flow_param = 1e4 # m horizontal / yr
 snow_fall = 0.5 # m / y
-
 steps = int(num_years / time_step)
 cell_width = domain_width / num_cells
 
@@ -17,17 +21,18 @@ elevations = np.zeros(num_cells + 2)
 flows = np.zeros(num_cells + 1)
 
 # get the plot started
+plt.style.use('seaborn')
 fig, ax = plt.subplots(figsize=(5, 3))
 ax.set_ylim(-10, 4000)
+ax.set_title(f'Years: ---')
+ax.set_ylabel('Elevation (m)')
+ax.set_xlabel('Cell Index')
 line = ax.plot(elevations, lw=2)[0]
 
-def init_func():
-    '''reset the elevations and flows'''
-    elevations[:] = 0
-    flows[:] = 0
-
 def update(i):
-
+    '''Insead a loop, I define this function,
+    which matplotlib will use to update the arrays and then the plot
+    '''
     # update flows
     for idx in range(len(flows)):
         slope = (elevations[idx] - elevations[idx + 1]) / cell_width
@@ -42,14 +47,25 @@ def update(i):
     ax.set_title(f'Years: {i * time_step}')
     line.set_ydata(elevations) # update line
 
+    # print per the prompt spec
+    print(f'Years: {i * time_step} | Elevation at center: {elevations[5]}')
+
+# this function gets used when the animation loops
+def reset_arrays():
+    '''Reset the elevations and flows.
+    '''
+    elevations[:] = 0
+    flows[:] = 0
+
 # tell matplotlib to animate
 anim = FuncAnimation(
     fig,
-    update,
-    init_func=init_func,
-    interval=10,
+    update, # defined above
+    init_func=reset_arrays, # defined above
+    interval=50, # this basically controls the speed of the playback
     frames=steps
 )
  
 plt.draw()
+plt.tight_layout()
 plt.show()
